6776f508f70be65588775b1b94ef6241f401abee
alvaroboni77 <alvarobonilla77@gmail.com>
2020-06-17 14:56:49 +0200

    Diseñar la vista para el listado de constructores #7

---
+++ src/DataFixtures/BuilderFixture.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Builder;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as FakerFactoryAlias;
+use Faker\Generator as FakerGeneratorAlias;
+
+class BuilderFixture extends Fixture
+{
+    /** @var FakerGeneratorAlias $faker */
+    private static $faker;
+
+    public function load(ObjectManager $manager)
+    {
+        self::$faker = FakerFactoryAlias::create('es_ES');
+
+        for ($i = 0; $i < 20; $i++) {
+            $builder = new Builder();
+            $builder
+                ->setName(self::$faker->name)
+                ->setEmail(self::$faker->email)
+                ->setCompany(self::$faker->company);
+            $manager->persist($builder);
+        }
+        $manager->flush();
+    }
+}
