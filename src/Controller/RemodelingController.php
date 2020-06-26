d30c971322d82df8d7f248f918635ff7d17f375a
alvaroboni77 <alvarobonilla77@gmail.com>
2020-06-26 18:47:24 +0200

    Implementar la lógica para ver, borrar y editar reformas #13

--- src/Controller/RemodelingController.php
+++ src/Controller/RemodelingController.php
@@ -20,9 +20,12 @@ class RemodelingController extends AbstractController
     /**
      * @Route("/", name="list")
      */
-    public function remodelingList()
+    public function list()
     {
-        return $this->render('remodeling/list.html.twig');
+        $remodelings = $this->getDoctrine()->getRepository(Remodeling::class)->findAll();
+        return $this->render('remodeling/list.html.twig', [
+            'remodelings' => $remodelings
+        ]);
     }
 
     /**
@@ -56,6 +59,68 @@ class RemodelingController extends AbstractController
         return $this->render('remodeling/new.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @param Request $request
+     * @param int $id
+     * @return RedirectResponse|Response
+     */
+    public function edit(Request $request, int $id)
+    {
+        $remodeling = $this->getDoctrine()->getRepository(Remodeling::class)->find($id);
+
+        $form = $this->createForm(RemodelingType::class, $remodeling);
+        $form->add('Update', SubmitType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $remodeling = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($remodeling);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'info',
+                '¡Proyecto modificado!'
+            );
+
+            return $this->redirectToRoute('remodeling_list');
+        }
+
+        return $this->render('remodeling/edit.html.twig', [
+            'remodeling' => $remodeling,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     * @param int $id
+     * @return Response
+     */
+    public function siteProjectDelete(int $id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $remodeling = $entityManager->getRepository(Remodeling::class)->find($id);
+
+        if(!$remodeling) {
+            throw $this->createNotFoundException(
+                'No builder found for id '.$id
+            );
+        }
+
+        $entityManager->remove($remodeling);
+        $entityManager->flush();
+
+        $this->addFlash(
+            'danger',
+            '¡Proyecto eliminado!'
+        );
+
+        return new Response('Remodeling deleted', 200);
     }
 
 }
